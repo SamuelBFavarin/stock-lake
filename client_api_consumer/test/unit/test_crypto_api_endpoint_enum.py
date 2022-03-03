f10294dbac0d8c1613a657391793dd07f82f994a
Samuel Brati Favarin <samuel.favarin@conectanuvem.com.br>
2022-03-03 20:56:07 -0300

    fix: remove a test line

--- client_api_consumer/test/unit/test_crypto_api_endpoint_enum.py
+++ client_api_consumer/test/unit/test_crypto_api_endpoint_enum.py
@@ -6,5 +6,3 @@ def test_is_endpoint_key_valid():
     assert EndpointEnum.is_endpoint_key_valid('GLOBAL_CRYPTOCURRENCY') is True
     assert EndpointEnum.is_endpoint_key_valid('TRENDING_COINS') is True
     assert EndpointEnum.is_endpoint_key_valid('THIS_IS_FUN') is False
-    assert EndpointEnum.is_endpoint_key_valid('THIS_IS_FUN_2') is False
-    
