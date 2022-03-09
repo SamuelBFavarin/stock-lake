f2326d715142adaeb3fa87d1111edddb59eaef88
Samuel Brati Favarin <samuel.favarin@conectanuvem.com.br>
2022-03-08 21:36:52 -0300

    feat: implements request function

--- client_api_consumer/crypto_api/crypto_api_consumer.py
+++ client_api_consumer/crypto_api/crypto_api_consumer.py
@@ -1,6 +1,8 @@
+from urllib import response
 from api_consumer import ApiConsumer
-from crypto_api_endpoint_enum import EndpointEnum
+from .crypto_api_endpoint_enum import EndpointEnum
 import json
+import requests
 
 
 class CryptoApiConsumer(ApiConsumer):
@@ -12,6 +14,40 @@ class CryptoApiConsumer(ApiConsumer):
     def set_credentials(self, api_token: str):
         self.api_token = api_token
 
-    def request(self, endpont: EndpointEnum, method: str, is_paginated: bool = False) -> json:
-        super().request(endpont, method, is_paginated)
-        pass
+    def request(self, endpoint: EndpointEnum, method: str, is_paginated: bool = False) -> json:
+        super().request(endpoint, method, is_paginated)
+
+        URL = f"""{self.base_url}{endpoint.value["endpoint"]}"""
+        resp = None
+
+        if method.upper() == "GET":
+            resp = requests.get(URL)
+
+        elif method.upper() == "POST":
+            resp = requests.post(URL)
+
+        else:
+            return {
+                "code": 405,
+                "message": "Method Not Allowed",
+            }
+
+        if self._check_is_success_status_code(resp):
+            return {
+                "code": resp.status_code,
+                "message": resp.reason,
+                "data": resp.json()[endpoint.value["records_key"]],
+            }
+
+        else:
+            return {
+                "code": resp.status_code,
+                "message": resp.reason,
+            }
+
+    def _check_is_success_status_code(self, resp: response) -> bool:
+
+        if resp.status_code >= 200 and resp.status_code <= 206:
+            return True
+
+        return False
